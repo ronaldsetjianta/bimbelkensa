6617665b0effbcef66b3316f1788f073e3f3d23a
RONALD <ronaldsetjianta@gmail.com>
2025-11-02 11:15:14 +0700

    Update: Display

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ import ScrollToTop from "./components/ScrollToTop";
 import useScrollAnimation from "./hooks/useScrollAnimation";
 import "./App.css";
 
+import { supabase } from "./supabaseClient";
+import LoginPage from "./login";
+import SignupPage from "./signup";
+
 import MatematikaPage from "./matematika";
 import FunctionAndGraphsPage from "./functionsandgraphs_lts25-b1";
 
@@ -27,10 +31,6 @@ import CommonIonsPage from "./commonions";
 
 import AkuntansiPage from "./akuntansi";
 import SahamBiasaPreferenPage from "./sahambiasapreferen";
-
-import { supabase } from "./supabaseClient";
-import LoginPage from "./login";
-import SignupPage from "./signup";
 
 const scrollToElement = (elementId) => {
   setTimeout(() => {
@@ -110,7 +110,17 @@ function Header({ session, profile }) {
             {session && profile ? (
               <>
                 <span className="user-greeting">Hai, {profile.nama}!</span>
-                <button onClick={handleLogout} className="header-auth-link">Logout</button>
+                {/* MODIFIKASI: Menggunakan <a> tag, bukan <button> */}
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleLogout();
+                  }}
+                  className="header-auth-link"
+                >
+                  Logout
+                </a>
               </>
             ) : (
               <>
@@ -236,7 +246,7 @@ function HomePage() {
             <a href="mailto:bimbelkensa@gmail.com" className="kontak-item-link">
               <div className="kontak-item">
                 <div className="kontak-icon">
-                  <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48"><path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/><path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6.36C43.92 37.63 46.98 31.48 46.98 24.55z"/><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/><path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6.36c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/><path fill="none" d="M0 0h48v48H0z"/></svg>
+                  <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48"><path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/><path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6.36C43.92 37.63 46.98 31.48 46.98 24.55z"/><path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/><path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6.36c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/><path fill="none" d="M0 0h48v48H0z"/></svg>
                 </div>
                 <h3>Email</h3>
                 <p>bimbelkensa@gmail.com</p>
@@ -306,9 +316,12 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
 
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+
             <Route path="/matematika" element={<MatematikaPage />} />
             <Route path="/functionsandgraphs_lts25-b1" element={<FunctionAndGraphsPage />} />
-         
+          
             <Route path="/fisika" element={<FisikaPage />} />
             <Route path="/listrikstatis_mar25-r1" element={<ListrikStatisPage />} />
             <Route path="/listrikdinamis_mar25-r1" element={<ListrikDinamisPage />} />
@@ -321,10 +334,7 @@ function App() {
 
             <Route path="/layanan-individu" element={<LayananIndividu />} />
             <Route path="/layanan-perusahaan" element={<LayananPerusahaan />} />
-            <Route path="/booking-konsultasi" element={<BookingKonsultasi />} />
-        
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/booking-konsultasi" element={<BookingKonsultasi />} />    
           </Routes>
         </main>
         <Footer />
